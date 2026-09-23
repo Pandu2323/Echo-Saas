db8e727b7218cf6037e7ca32446a31f80bacccd0
Prashanthsyntax <pendemprashanth49@gmail.com>
2026-09-23 22:16:40 +0530

    Fixed: Warnings

--- src/components/sentracode/attack-graph.tsx
+++ src/components/sentracode/attack-graph.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/set-state-in-effect */
 /* eslint-disable react-hooks/refs */
 /* eslint-disable @typescript-eslint/no-explicit-any */
